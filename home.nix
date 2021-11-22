4b7a143b3c08fd46a0f423c31a1f76b08155738e
Shawn Koh <shawn@shawnkoh.sg>
2021-11-23 01:19:28 +0800

    Add nushell

--- home.nix
+++ home.nix
@@ -121,6 +121,14 @@
 
   programs.nushell = {
     enable = true;
+    settings = {
+      startup = [
+        "mkdir ~/.cache/starship",
+        "starship init nu | save ~/.cache/starship/init.nu",
+        "source ~/.cache/starship/init.nu"
+      ];
+      prompt = "starship_prompt";
+    };
   };
 
   # programs.fish.plugins = [{
