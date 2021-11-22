91d0813c77cb1a34cd37a77b0885f1887d263e80
Shawn Koh <shawn@shawnkoh.sg>
2021-11-22 18:54:11 +0800

    ADd nix-env.fish

--- home.nix
+++ home.nix
@@ -54,6 +54,15 @@
     # Temporary workaround
     plugins = [
       {
+        name = "nix-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "00c6cc762427efe08ac0bd0d1b1d12048d3ca727";
+          sha256 = "1hrl22dd0aaszdanhvddvqz3aq40jp9zi2zn0v1hjnf7fx4bgpma";
+        };
+      }
+      {
         name = "iterm2-shell-integration";
         src = ./config/fish/iterm2_shell_integration;
       }
