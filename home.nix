2ed26dd43d7c6272e001bfb9c60fbdfa1f45c42f
Shawn Koh <shawn@shawnkoh.sg>
2021-11-23 01:19:56 +0800

    Typo

--- home.nix
+++ home.nix
@@ -123,8 +123,8 @@
     enable = true;
     settings = {
       startup = [
-        "mkdir ~/.cache/starship",
-        "starship init nu | save ~/.cache/starship/init.nu",
+        "mkdir ~/.cache/starship"
+        "starship init nu | save ~/.cache/starship/init.nu"
         "source ~/.cache/starship/init.nu"
       ];
       prompt = "starship_prompt";
