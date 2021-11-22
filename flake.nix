de26fa5cb2e56b203388531c85847a48317437bd
Shawn Koh <shawn@shawnkoh.sg>
2021-11-22 12:15:28 +0800

    Move home manager properties into flake.nix

--- flake.nix
+++ flake.nix
@@ -17,4 +17,21 @@
       };
     };
   };
+
+  # Note: This was moved from home.nix
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "shawnkoh";
+  home.homeDirectory = "/home/shawnkoh";
+
+  # Note: This was moved from home.nix
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
 }
