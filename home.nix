9bd63401a6a744077465913d7e9610ad608fb7fd
Shawn Koh <shawn@shawnkoh.sg>
2021-11-23 08:57:48 +0800

    Remove nushell

--- home.nix
+++ home.nix
@@ -119,18 +119,6 @@
     enableFishIntegration = true;
   };
 
-  programs.nushell = {
-    enable = true;
-    settings = {
-      startup = [
-        "mkdir ~/.cache/starship"
-        "starship init nu | save ~/.cache/starship/init.nu"
-        "source ~/.cache/starship/init.nu"
-      ];
-      prompt = "starship_prompt";
-    };
-  };
-
   # programs.fish.plugins = [{
   #   name = "fzf.fish";
   #   src = pkgs.fetchFromGitHub {
