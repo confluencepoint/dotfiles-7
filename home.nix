0e1ad856447f08a8145932d33b7e32501b825a07
Shawn Koh <shawn@shawnkoh.sg>
2021-11-24 19:05:37 +0800

    Fix

--- home.nix
+++ home.nix
@@ -97,11 +97,7 @@
 
   programs.ssh = {
     enable = true;
-    extraConfig = "
-      AddKeysToAgent yes
-      UseKeyChain yes
-      IdentityFile ~/.ssh/id_ed25519
-    ";
+    extraConfig = "AddKeysToAgent yes\nUseKeyChain yes\nIdentityFile ~/.ssh/id_ed25519";
   };
 
   programs.git = {
